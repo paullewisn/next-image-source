3dec73311918bec8c69ae9709ece0a23ffa14eb7
Paul-Lewis Nunn <paullewisn@users.noreply.github.com>
2024-06-19 09:56:03 +0100

    Add PictureImage component and imgixLoader function to d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,8 +1,9 @@
-declare module 'nextImageSource' {}
-
 export type {
   PictureImageProps,
   SourcesType,
 } from './src/PictureImage.types';
 
 export type { Loader, LoaderOpts } from './src/loaders/loaders.type';
+
+declare function PictureImage(props: PictureImageProps): JSX.Element;
+declare function imgixLoader(src: string, opts: LoaderOpts): string;
