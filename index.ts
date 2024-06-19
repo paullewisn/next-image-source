a6de6459826c825ab82e70f7b2faa3af9a1aa031
Paul-Lewis Nunn <paullewisn@users.noreply.github.com>
2024-06-19 10:26:31 +0100

    chore: Update package version to 0.0.4, build index.js, and add PictureImage component and imgixLoader function to d.ts

--- index.ts
+++
@@ -1,5 +0,0 @@
-export type * from './src/PictureImage.types';
-export { PictureImage } from './src/PictureImage';
-
-export type * from './src/loaders/loaders.type';
-export { imgixLoader } from './src/loaders/imgix';
