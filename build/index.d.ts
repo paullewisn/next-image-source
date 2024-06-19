d5aa9ff134bcafc30bc59eafe97a859747474c38
Paul-Lewis Nunn <paullewisn@users.noreply.github.com>
2024-06-19 10:40:18 +0100

    chore: Update imgixLoader function signature to accept optional LoaderOpts

--- build/index.d.ts
+++ build/index.d.ts
@@ -6,7 +6,7 @@ import type {
 import type { Loader, LoaderOpts } from './src/loaders/loaders.type';
 
 declare function PictureImage(props: PictureImageProps): JSX.Element;
-declare function imgixLoader(src: string, opts: LoaderOpts): string;
+declare function imgixLoader(src: string, opts?: LoaderOpts): string;
 
 export { PictureImage, imgixLoader };
 export type { Loader, LoaderOpts, PictureImageProps, SourcesType };
